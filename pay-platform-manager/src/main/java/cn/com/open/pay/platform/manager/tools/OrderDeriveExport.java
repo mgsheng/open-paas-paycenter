331c53cce2220496b8f90b8e2216242d310f4b2d
devsunhanjp <devsunhanjp@localhost>
2016-08-17 05:05:37 +0000

    查询下载

--- pay-platform-manager/src/main/java/cn/com/open/pay/platform/manager/tools/OrderDeriveExport.java
+++ pay-platform-manager/src/main/java/cn/com/open/pay/platform/manager/tools/OrderDeriveExport.java
@@ -41,85 +41,97 @@ public class OrderDeriveExport {
 		style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
 		// 给表头第一行一次创建单元格
 		HSSFCell cell = row.createCell((short) 0);
-		cell.setCellValue("下单时间");
+		cell.setCellValue("订单号");
 		cell.setCellStyle(style);
 		cell = row.createCell((short) 1);
-		cell.setCellValue("交易时间");
+		cell.setCellValue("商户订单号");
 		cell.setCellStyle(style);
 		cell = row.createCell((short) 2);
-		cell.setCellValue("商户订单号");
+		cell.setCellValue("第三方订单号");
 		cell.setCellStyle(style);
 		cell.setCellStyle(style);
 		cell = row.createCell((short) 3);
-		cell.setCellValue("第三方订单号");
+		cell.setCellValue("下单时间");
 		cell.setCellStyle(style);
 		cell.setCellStyle
 
 		(style);
 		cell = row.createCell((short) 4);
-		cell.setCellValue("支付方式");
+		cell.setCellValue("交易时间");
 		cell.setCellStyle(style);
 		cell.setCellStyle
 
 		(style);
 		cell = row.createCell((short) 5);
-		cell.setCellValue("发卡行");
+		cell.setCellValue("支付方式");
 		cell.setCellStyle(style);
 		cell.setCellStyle
 
 		(style);
 		cell = row.createCell((short) 6);
-		cell.setCellValue("卡类型");
+		cell.setCellValue("业务类型");
 		cell.setCellStyle(style);
 		cell.setCellStyle
 
 		(style);
 		cell = row.createCell((short) 7);
-		cell.setCellValue("缴费来源");
+		cell.setCellValue("发卡行");
 		cell.setCellStyle(style);
 		cell.setCellStyle
 
 		(style);
 		cell = row.createCell((short) 8);
-		cell.setCellValue("交易状态");
+		cell.setCellValue("卡类型");
 		cell.setCellStyle(style);
 		cell.setCellStyle
 
 		(style);
 		cell = row.createCell((short) 9);
-		cell.setCellValue("缴费金额");
+		cell.setCellValue("缴费来源");
 		cell.setCellStyle(style);
 		cell.setCellStyle
 
 		(style);
 		cell = row.createCell((short) 10);
-		cell.setCellValue("实收金额");
+		cell.setCellValue("交易状态");
 		cell.setCellStyle(style);
 		cell.setCellStyle
 
 		(style);
 		cell = row.createCell((short) 11);
+		cell.setCellValue("缴费金额");
+		cell.setCellStyle(style);
+		cell.setCellStyle
+
+		(style);
+		cell = row.createCell((short) 12);
+		cell.setCellValue("实收金额");
+		cell.setCellStyle
+		
+		(style);
+		cell = row.createCell((short) 13);
 		cell.setCellValue("手续费");
 		cell.setCellStyle(style);
-		
 		/*cell = row.createCell((short) 11);
 		cell.setCellValue("人才使用");
 		cell.setCellStyle(style);*/
 		// 向单元格里填充数据
 		for (short i = 0; i < infoList.size(); i++) {
 			row = sheet.createRow(i + 1);
-			row.createCell(0).setCellValue(infoList.get(i).getMerchantOrderDate());//下单时间
-			row.createCell(1).setCellValue(infoList.get(i).getMerchantOrderDate());//交易时间 0
-			row.createCell(2).setCellValue(infoList.get(i).getMerchantOrderId());//商户订单号
-			row.createCell(3).setCellValue(infoList.get(i).getPayOrderId());//第三方订单号
-			row.createCell(4).setCellValue(infoList.get(i).getPayOrderId());//支付方式 0
-			row.createCell(5).setCellValue(infoList.get(i).getChannelOrderId());//发卡行 0
-			row.createCell(6).setCellValue(infoList.get(i).getChannelOrderId());//卡类型 0
-			row.createCell(7).setCellValue(infoList.get(i).getChannelOrderId());//缴费来源 0
-			row.createCell(8).setCellValue(infoList.get(i).getPayStatus());//缴费状态
-			row.createCell(9).setCellValue(infoList.get(i).getAmount());//缴费金额
-			row.createCell(10).setCellValue(infoList.get(i).getPayAmount());//实收金额
-			row.createCell(11).setCellValue(infoList.get(i).getPayCharge());//手续费
+			row.createCell(0).setCellValue(infoList.get(i).getId());//下单时间
+			row.createCell(1).setCellValue(infoList.get(i).getMerchantOrderId());//商户订单号
+			row.createCell(2).setCellValue(infoList.get(i).getPayOrderId());//第三方订单号
+			row.createCell(3).setCellValue(infoList.get(i).getFoundDate());//下单时间 
+			row.createCell(4).setCellValue(infoList.get(i).getBusinessDate());//交易时间
+			row.createCell(5).setCellValue(infoList.get(i).getChannelName());//支付方式
+			row.createCell(6).setCellValue(infoList.get(i).getAppId());//业务类型
+			row.createCell(7).setCellValue(infoList.get(i).getPaymentName());//发卡行
+			row.createCell(8).setCellValue(infoList.get(i).getPaymentName());//卡类型000
+			row.createCell(9).setCellValue(infoList.get(i).getPaymentName());//缴费来源000
+			row.createCell(10).setCellValue(infoList.get(i).getPayStatusName());//交易状态
+			row.createCell(11).setCellValue(infoList.get(i).getAmount());//缴费金额
+			row.createCell(12).setCellValue(infoList.get(i).getPayAmount());//实收金额
+			row.createCell(13).setCellValue(infoList.get(i).getPayCharge());//手续费
 			//人才使用总和
 			//String thingName="";
 			/*String orderState="4";
