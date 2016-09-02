b860117b19a1ed73f0e014acabc279a7211d32b0
devsunhanjp <devsunhanjp@localhost>
2016-09-02 09:08:18 +0000

    对账单查询下载

--- pay-serviceimpl/src/main/java/cn/com/open/openpaas/payservice/web/api/order/UserOrderQueryController.java
+++ pay-serviceimpl/src/main/java/cn/com/open/openpaas/payservice/web/api/order/UserOrderQueryController.java
@@ -218,87 +218,7 @@ public class UserOrderQueryController extends BaseControllerUtil{
 		}
 	}
     
-    /**fileDownlond
-     * 对账文件下载申请
-     * @throws Exception 
-     */
-    @RequestMapping(value = "fileDownlond")
-    public void fileDownlond(HttpServletRequest request,HttpServletResponse response,Model model) throws Exception{
-    	String outTradeNo=request.getParameter("outTradeNo");
-        String appId = request.getParameter("appId");
-        String timestamp=request.getParameter("timestamp");
-	    String signatureNonce=request.getParameter("signatureNonce");
-	    String signature=request.getParameter("signature");
-      //获取当前订单
-  		MerchantOrderInfo orderInfo = merchantOrderInfoService.findByMerchantOrderId(outTradeNo,appId);
-  		
-  		if(orderInfo==null){
-         	paraMandaChkAndReturn(1, response,"订单号不存在");
-         	return ;
-         } 
-  		MerchantInfo merchantInfo=merchantInfoService.findById(orderInfo.getMerchantId());
-  		if(merchantInfo==null){
-         	paraMandaChkAndReturn(2, response,"认证失败");
-         	return ;
-         } 
-  		SortedMap<Object,Object> sParaTemp = new TreeMap<Object,Object>();
-    	sParaTemp.put("appId",appId);
-   		sParaTemp.put("timestamp", timestamp);
-   		sParaTemp.put("signatureNonce", signatureNonce);
-   		sParaTemp.put("outTradeNo",outTradeNo );
-   		String params=createSign(sParaTemp);
-   	    Boolean hmacSHA1Verification=OauthSignatureValidateHandler.validateSignature(signature,params,merchantInfo.getPayKey());
-        //认证
-       // Boolean hmacSHA1Verification=OauthSignatureValidateHandler.validateSignature(request,merchantInfo);
-		if(!hmacSHA1Verification){
-			paraMandaChkAndReturn(2, response,"认证失败");
-			return;
-		} 
-    			DictTradeChannel dictTradeChannels=dictTradeChannelService.findByMAI(String.valueOf(orderInfo.getMerchantId()),Channel.TCL.getValue());
-    			Map<String, String> orderQryDataMap1 = OrderQryData.buildGetFileDownloadMap(orderInfo, dictTradeChannels);
-    			Map<String,String> sParaTemp2 = new HashMap<String,String>();
-    			String other= dictTradeChannels.getOther();
-    			Map<String, String> others = new HashMap<String, String>();
-    			others=getPartner(other);
-    			String merchant_code=others.get("merchant_code");
-				String acDate = HytDateUtils.getCurDateStr();
-				sParaTemp2.put(HytParamKeys.CHARSET,"00");
-				sParaTemp2.put(HytParamKeys.VERSION, "1.0");
-				sParaTemp2.put(HytParamKeys.SIGN_TYPE,"RSA");
-				sParaTemp2.put(HytParamKeys.SERVICE, "Statement");
-				sParaTemp2.put(HytParamKeys.REQUEST_ID,  orderInfo.getPayOrderId());
-				sParaTemp2.put(HytParamKeys.MERCHANT_CODE, merchant_code);
-				sParaTemp2.put(HytParamKeys.AC_DATE, acDate);
-				RSASign util = HytUtils.getRSASignObject();
-	   			 String reqData = HytPacketUtils.map2Str(sParaTemp2);
-	   			 String merchant_sign = "";
-	   				try {
-	   					merchant_sign = util.sign(reqData, HytConstants.CHARSET_GBK);
-	   				} catch (Exception e) {
-	   					e.printStackTrace();
-	   				}
-					String merchant_cert = util.getCertInfo();
-					sParaTemp2.put(HytParamKeys.MERCHANT_CERT, merchant_cert);
-					sParaTemp2.put(HytParamKeys.MERCHANT_SIGN, merchant_sign);
-				String result=sendPost1("https://ipos.tclpay.cn/hipos/payTrans", sParaTemp2);
-				sParaTemp2.remove(HytParamKeys.MERCHANT_CERT);
-				sParaTemp2.remove(HytParamKeys.MERCHANT_SIGN);
-    			String Wsign=HytPacketUtils.map2StrRealURL(sParaTemp2);
-    			boolean flag = false;
-    			RSASign rsautil =HytUtils.getRSASignVertifyObject(); 
-    			flag = rsautil.verify(Wsign,merchant_sign,merchant_cert, HytConstants.CHARSET_GBK);//验证签名
-    			Map<String, String> othere = new HashMap<String, String>();
-    			if(flag!=true){
-    				othere.put("false", "验证没通过查询失败");
-    				writeSuccessJson(response,othere);
-    			}else{
-        			othere=getDownlond(result);
-        			JSONObject obj = JSONObject.fromObject(othere);
-        			System.out.println(obj);
-        			writeSuccessJson(response,obj);
-    			}
-    			
-    }
+    
     
     public static Map<String, String> getDownlond(String other){
 		if(other==null&&"".equals(other)){
@@ -345,4 +265,84 @@ public class UserOrderQueryController extends BaseControllerUtil{
   			return sParaTemp;
   			}
   		}
+  		
+  		
+  		/**fileDownlond
+  	     * 对账文件下载申请
+  	     * @throws Exception 
+  	     */
+  	    @RequestMapping(value = "fileDownlond")
+  	    public void fileDownlond(HttpServletRequest request,HttpServletResponse response,Model model) throws Exception{
+//  	    	String outTradeNo=request.getParameter("outTradeNo");
+  	        String appId = request.getParameter("appId");
+  	        String timestamp=request.getParameter("timestamp");
+  		    String signatureNonce=request.getParameter("signatureNonce");
+  		    String signature=request.getParameter("signature");  
+  		    String acDate=request.getParameter("acDate");
+  		    String merchantId=request.getParameter("merchantId");
+			 if(!paraMandatoryCheck(Arrays.asList(acDate,appId,merchantId))){
+				  	paraMandaChkAndReturn(2, response,"必传参数中有空值");
+				  	return ;
+		     }
+  	  		MerchantInfo merchantInfo=merchantInfoService.findById(Integer.parseInt(merchantId));
+  	  		if(merchantInfo==null){
+  	         	paraMandaChkAndReturn(2, response,"认证失败");
+  	         	return ;
+  	         } 
+  	  		SortedMap<Object,Object> sParaTemp = new TreeMap<Object,Object>();
+  	    	sParaTemp.put("appId",appId);
+  	   		sParaTemp.put("timestamp", timestamp);
+  	   		sParaTemp.put("signatureNonce", signatureNonce);
+  	   		sParaTemp.put("acDate",acDate );
+  	   		sParaTemp.put("merchantId",merchantId );
+  	   		String params=createSign(sParaTemp);
+  	   	    Boolean hmacSHA1Verification=OauthSignatureValidateHandler.validateSignature(signature,params,merchantInfo.getPayKey());
+  	        //认证
+  			if(!hmacSHA1Verification){
+  				paraMandaChkAndReturn(2, response,"认证失败");
+  				return;
+  			} 
+  	    			DictTradeChannel dictTradeChannels=dictTradeChannelService.findByMAI(merchantId,Channel.TCL.getValue());
+  	    			Map<String,String> sParaTemp2 = new HashMap<String,String>();
+  	    			String other= dictTradeChannels.getOther();
+  	    			Map<String, String> others = new HashMap<String, String>();
+  	    			others=getPartner(other);
+  	    			String merchant_code=others.get("merchant_code");
+  					sParaTemp2.put(HytParamKeys.CHARSET,"00");							//字符集
+  					sParaTemp2.put(HytParamKeys.VERSION, "1.0");						//接口版本
+  					sParaTemp2.put(HytParamKeys.SIGN_TYPE,"RSA");						//签名方式
+  					sParaTemp2.put(HytParamKeys.SERVICE, "Statement");					//业务类型
+  					sParaTemp2.put(HytParamKeys.REQUEST_ID,  "1");						//商户请求号 暂定
+  					sParaTemp2.put(HytParamKeys.MERCHANT_CODE, merchant_code);			//商户编号
+  					sParaTemp2.put(HytParamKeys.AC_DATE, acDate); 						//会计日期
+  					RSASign util = HytUtils.getRSASignObject();
+  		   			 String reqData = HytPacketUtils.map2Str(sParaTemp2);
+  		   			 String merchant_sign = "";
+  		   				try {
+  		   					merchant_sign = util.sign(reqData, HytConstants.CHARSET_GBK);
+  		   				} catch (Exception e) {
+  		   					e.printStackTrace();
+  		   				}
+  						String merchant_cert = util.getCertInfo();
+  						sParaTemp2.put(HytParamKeys.MERCHANT_CERT, merchant_cert);		//商户证书
+  						sParaTemp2.put(HytParamKeys.MERCHANT_SIGN, merchant_sign);		//商户签名
+  					String result=sendPost1("https://ipos.tclpay.cn/hipos/payTrans", sParaTemp2);
+  					sParaTemp2.remove(HytParamKeys.MERCHANT_CERT);
+  					sParaTemp2.remove(HytParamKeys.MERCHANT_SIGN);
+  	    			String Wsign=HytPacketUtils.map2StrRealURL(sParaTemp2);
+  	    			boolean flag = false;
+  	    			RSASign rsautil =HytUtils.getRSASignVertifyObject(); 
+  	    			flag = rsautil.verify(Wsign,merchant_sign,merchant_cert, HytConstants.CHARSET_GBK);//验证签名
+  	    			Map<String, String> othere = new HashMap<String, String>();
+  	    			if(flag!=true){
+  	    				othere.put("false", "验证没通过查询失败");
+  	    				writeSuccessJson(response,othere);
+  	    			}else{
+  	        			othere=getDownlond(result);
+  	        			JSONObject obj = JSONObject.fromObject(othere);
+  	        			System.out.println(obj);
+  	        			writeSuccessJson(response,obj);
+  	    			}
+  	    			
+  	    }
 }
