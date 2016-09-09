93c8688019f87dd2f595629308a98960b4cbe807
lvjq <lvjq@localhost>
2016-09-09 02:10:36 +0000

    用户增删改查

--- pay-platform-manager/src/main/java/cn/com/open/pay/platform/manager/web/ManagerUserController.java
+++ pay-platform-manager/src/main/java/cn/com/open/pay/platform/manager/web/ManagerUserController.java
@@ -1,13 +1,16 @@
 package cn.com.open.pay.platform.manager.web;
 
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONObject;
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,14 +38,9 @@ public class ManagerUserController  extends BaseControllerUtil {
 	@RequestMapping("updateUser")
 	public void updateUserByID(HttpServletRequest request,HttpServletResponse response)throws UnsupportedEncodingException{
 		log.info("-------------------------updateUserByID        start------------------------------------");
-		String Id = request.getParameter("id");
-		String realname = request.getParameter("realname");
-		String nickname = request.getParameter("nickname");
-		Id = new String(Id.getBytes("iso-8859-1"),"utf-8");
-		realname = new String(realname.getBytes("iso-8859-1"),"utf-8");
-		nickname = new String(nickname.getBytes("iso-8859-1"),"utf-8");
-//		System.out.println(Id+":"+realname+":"+nickname);
-		Integer id = Integer.valueOf(Id);
+		String realname = new String(request.getParameter("realname").getBytes("iso-8859-1"),"utf-8");
+		String nickname = new String(request.getParameter("nickname").getBytes("iso-8859-1"),"utf-8");
+		Integer id = Integer.valueOf(new String(request.getParameter("id").getBytes("iso-8859-1"),"utf-8"));
 		
 		//将请求参数封装到User对象中
 		User user = new User();
@@ -58,9 +56,6 @@ public class ManagerUserController  extends BaseControllerUtil {
 		return;
 	}
 	
-	
-	
-	
 	/**
 	 * 根据用户id删除用户
 	 * @param request
@@ -68,11 +63,8 @@ public class ManagerUserController  extends BaseControllerUtil {
 	 */
 	@RequestMapping("removeUserByID")
 	public void removeUser(HttpServletRequest request,HttpServletResponse response)throws UnsupportedEncodingException{
-		String id = request.getParameter("id");
-		if(id != null || id != ""){
-			id = new String(id.getBytes("iso-8859-1"),"utf-8");
-		}
-		boolean result = userService.removeUserByID(Integer.valueOf(id));
+		Integer	id = Integer.valueOf(new String(request.getParameter("id").getBytes("iso-8859-1"),"utf-8"));
+		boolean result = userService.removeUserByID(id);
 		// result = 1表示该用户删除成功
 		JSONObject jsonobj = new JSONObject();
 		jsonobj.put("result",result);
@@ -99,45 +91,40 @@ public class ManagerUserController  extends BaseControllerUtil {
 	@RequestMapping("findUsers")
 	public void findUsers(HttpServletRequest request,HttpServletResponse response)throws UnsupportedEncodingException{
 		log.info("-------------------------findUsers        start------------------------------------");
-		String username = request.getParameter("username");
-		String realname = request.getParameter("realname");
-		String nickname = request.getParameter("nickname");
-		username = new String(username.getBytes("iso-8859-1"),"utf-8");
-		realname = new String(realname.getBytes("iso-8859-1"),"utf-8");
-		nickname = new String(nickname.getBytes("iso-8859-1"),"utf-8");
+		String username = new String(request.getParameter("username").getBytes("iso-8859-1"),"utf-8");
+		String realname = new String(request.getParameter("realname").getBytes("iso-8859-1"),"utf-8");
+		String nickname = new String(request.getParameter("nickname").getBytes("iso-8859-1"),"utf-8");
 		//将请求参数封装到User对象中
 		User user = new User();
 		user.setUsername(username);
 		user.setRealName(realname);
 		user.setNickName(nickname);
 //		System.out.println("-----------username："+username+"realname："+realname+"nickname："+nickname+"-----------");
-		List<User> users = null;
-		users = userService.findUsers(user);
+		List<User> users = userService.findUsers(user);
+		if(users ==null) return;
 		int count = users.size();
-//		for(User us : users){
-//			System.out.println(us+":"+count);
+//		for(User u : users){
+//			System.out.println(u.getCreate_Time());
 //		}
-		 JSONArray jsonArr = JSONArray.fromObject(users);
-		 JSONObject jsonObjArr = new JSONObject();  
-		 jsonObjArr.put("total", count);
-		 jsonObjArr.put("rows", jsonArr);
-//		 System.out.println(jsonArr);
-	     WebUtils.writeJson(response,jsonObjArr);
+		
+		JSONArray jsonArr = JSONArray.fromObject(users);
+		JSONObject jsonObjArr = new JSONObject();  
+		jsonObjArr.put("total", count);
+		jsonObjArr.put("rows", jsonArr);
+//		System.out.println(jsonArr);
+	    WebUtils.writeJson(response,jsonObjArr);
 		return;
 	}
-	
 	
 	/**
 	 * 跳转到添加用户的页面
 	 * @return 返回的是 jsp文件名路径及文件名
 	 */
-	@RequestMapping(value="showAdd")
+	@RequestMapping(value="showAddUser")
 	public String showAdd(){
 		log.info("-------------------------showAdd         start------------------------------------");
-		return "show/add";
+		return "show/addUser";
 	}
-	
-	
 	
 	/**
 	 * 实现添加用户的操作
@@ -155,7 +142,7 @@ public class ManagerUserController  extends BaseControllerUtil {
 		String real_name = request.getParameter("real_name");
 		String nickname = request.getParameter("nickname");
 		String sha_password = request.getParameter("sha_password");
-		log.info("--------------user_name："+user_name+"；real_name:"+real_name+"；nickname:"+nickname+"；sha_password:"+sha_password+"-------------------------");
+//		log.info("--------------user_name："+user_name+"；real_name:"+real_name+"；nickname:"+nickname+"；sha_password:"+sha_password+"-------------------------");
 		JSONObject jsonObjArr = new JSONObject();  
 		//判断数据库是否已经存在该用户   result 为状态  0表示添加失败  1表示该用户已被注册   2 表示添加用户成功
 		boolean result = false;
@@ -171,7 +158,7 @@ public class ManagerUserController  extends BaseControllerUtil {
 		user.setUsername(user_name);
 		user.setRealName(real_name);
 		user.setNickName(nickname);
-		
+		user.setCreateTime(new Date().getTime());
 		//MD5加密
 		user.setPlanPassword(sha_password);
 		result = userService.addUser(user);
