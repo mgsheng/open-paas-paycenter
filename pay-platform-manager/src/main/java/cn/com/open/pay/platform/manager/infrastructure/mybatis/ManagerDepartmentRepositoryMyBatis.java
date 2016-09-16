582f4811bdaac3a43c7079ddf9e6cf0d157db2db
lvjq <lvjq@localhost>
2016-09-16 11:13:36 +0000

    部门SQL映射

---
+++ pay-platform-manager/src/main/java/cn/com/open/pay/platform/manager/infrastructure/mybatis/ManagerDepartmentRepositoryMyBatis.java
@@ -0,0 +1,11 @@
+package cn.com.open.pay.platform.manager.infrastructure.mybatis;
+
+import cn.com.open.pay.platform.manager.infrastructure.repository.ManagerDepartmentRepository;
+/**
+ * 部门管理
+ * @author lvjq
+ *
+ */
+public interface ManagerDepartmentRepositoryMyBatis extends ManagerDepartmentRepository{
+
+}
