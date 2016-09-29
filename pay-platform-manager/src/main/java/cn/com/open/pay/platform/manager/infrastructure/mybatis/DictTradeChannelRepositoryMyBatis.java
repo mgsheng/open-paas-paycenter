c27582a55aef8195fd06871c80a0fe703d5fc5a7
devsunhanjp <devsunhanjp@localhost>
2016-09-29 02:50:06 +0000

    渠道 商户

---
+++ pay-platform-manager/src/main/java/cn/com/open/pay/platform/manager/infrastructure/mybatis/DictTradeChannelRepositoryMyBatis.java
@@ -0,0 +1,10 @@
+package cn.com.open.pay.platform.manager.infrastructure.mybatis;
+
+import cn.com.open.pay.platform.manager.infrastructure.repository.DictTradeChannelRepository;
+
+
+/**
+ * 
+ */
+public interface DictTradeChannelRepositoryMyBatis extends DictTradeChannelRepository {
+}
---
+++ pay-platform-manager/src/main/java/cn/com/open/pay/platform/manager/infrastructure/mybatis/MerchantInfoRepositoryMyBatis.java
@@ -0,0 +1,10 @@
+package cn.com.open.pay.platform.manager.infrastructure.mybatis;
+
+import cn.com.open.pay.platform.manager.infrastructure.repository.MerchantInfoRepository;
+
+
+/**
+ * 
+ */
+public interface MerchantInfoRepositoryMyBatis extends MerchantInfoRepository {
+}
