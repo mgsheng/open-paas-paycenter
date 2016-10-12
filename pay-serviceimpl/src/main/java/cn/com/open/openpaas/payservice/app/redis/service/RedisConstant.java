7718edc3707881feb92a18913af3fc5c8f4b36fd
wangshuaia <wangshuaia@localhost>
2016-10-12 05:39:35 +0000

    添加易宝扫码支付

--- pay-serviceimpl/src/main/java/cn/com/open/openpaas/payservice/app/redis/service/RedisConstant.java
+++ pay-serviceimpl/src/main/java/cn/com/open/openpaas/payservice/app/redis/service/RedisConstant.java
@@ -8,4 +8,6 @@ public class RedisConstant {
  public static final String APP_INFO="appInfo_";
  public static final String USER_NAME_CHECK="userNameCheck_";
  public static final String USER_CACHE_INFO="userCacheInfo_";//存储方式：key:userCacheInfo_username value:""
+ public static final String ORDER_ID="orderId_";
+ public static final String ORDER_SCANCODE="orderScanCode_";
 }
