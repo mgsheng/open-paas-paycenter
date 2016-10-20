1efabfc74e89e0362f1f6a71b41b29c5159b6698
wangshuaia <wangshuaia@localhost>
2016-10-20 05:26:06 +0000

    修改通知金额

--- pay-order-service/src/main/java/cn/com/open/pay/order/service/web/OrderAutoSendController.java
+++ pay-order-service/src/main/java/cn/com/open/pay/order/service/web/OrderAutoSendController.java
@@ -77,7 +77,7 @@ public class OrderAutoSendController extends BaseControllerUtil{
 					params.put("appUid",String.valueOf(orderInfo.getSourceUid()));
 					//sParaTemp.put("exter_invoke_ip",exter_invoke_ip);
 					params.put("timeEnd", DateUtils.toDateText(new Date(), "yyyyMMddHHmmss"));
-					params.put("totalFee", String.valueOf((int)(orderInfo.getPayAmount()*100)));
+					params.put("totalFee", String.valueOf((int)(orderInfo.getOrderAmount()*100)));
 					params.put("goodsId", orderInfo.getMerchantProductId());
 					params.put("goodsName",orderInfo.getMerchantProductName());
 					params.put("goodsDesc", orderInfo.getMerchantProductDesc());
