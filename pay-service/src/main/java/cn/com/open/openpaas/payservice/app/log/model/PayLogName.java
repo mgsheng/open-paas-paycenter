4c9af9640dbdbbb5a5a3dd6f1b2cae59eaf84158
wangshuaia <wangshuaia@localhost>
2016-10-24 02:06:26 +0000

    修改通知时重复记录

--- pay-service/src/main/java/cn/com/open/openpaas/payservice/app/log/model/PayLogName.java
+++ pay-service/src/main/java/cn/com/open/openpaas/payservice/app/log/model/PayLogName.java
@@ -13,6 +13,18 @@ public interface PayLogName {
 	public static String CALLBACK_START = "callback_start";
 	//第三方回调结束
 	public static String CALLBACK_END = "callback_end";
+	//TCL回调开始
+	public static String TCL_CALLBACK_START = "tcl_callback_start";
+	//TCL回调结束
+	public static String TCL_CALLBACK_END = "tcl_callback_end";
+	//TCL回调开始
+	public static String TCL_NOTIFY_START = "tcl_notify_start";
+	//TCL回调结束
+	public static String TCL_NOTIFY_END = "tcl_notify_end";
+	//TCL回调开始
+	public static String WEIXIN_NOTIFY_START = "weixin_notify_start";
+	//TCL回调结束
+	public static String WEIXIN_NOTIFY_END = "weixin_notify_end";
 	
 	//第三方回调开始
 	public static String CALLBACK_NOTIFY_START = "callback_notify_start";
@@ -22,19 +34,41 @@ public interface PayLogName {
 	public static String ALIPAY_RETURN_START = "alipay_return_start";
 	//支付宝页面回调结束
 	public static String ALIPAY_RETURN_END = "alipay_return_end";
+	//支付宝回调开始
+	public static String ALIPAY_NOTIFY_START = "alipay_notify_start";
+	//支付宝回调结束
+	public static String ALIPAY_NOTIFY_END = "alipay_notify_end";
+	//易宝回调开始
+	public static String YEEPAY_NOTIFY_START = "yeepay_notify_start";
+	//易宝回调结束
+	public static String YEEPAY_NOTIFY_END = "yeepay_notify_end";
+	//易宝回调结束
+	public static String YEEPAY_CALLBACK_START = "yeepay_callback_start";
+	//易宝回调结束
+	public static String YEEPAY_CALLBACK_END = "yeepay_callback_end";
 	
 	//拉卡拉页面回调开始
 	public static String PAYMAX_RETURN_START = "paymax_return_start";
 	//拉卡拉页面回调结束
 	public static String PAYMAX_RETURN_END = "paymax_return_end";
 	//易宝页面回调开始
-	public static String YEEPAY_RETURN_START = "paymax_return_start";
+	public static String YEEPAY_RETURN_START = "yeepay_return_start";
 	//易宝页面回调结束
-	public static String YEEPAY_RETURN_END = "paymax_return_end";
+	public static String YEEPAY_RETURN_END = "yeepay_return_end";
 	//拉卡拉异步回调开始
 	public static String PAYMAX_NOTIFY_START = "paymax_notify_start";
 	//拉卡拉异步回调结束
 	public static String PAYMAX_NOTIFY_END = "paymax_notify_end";
+	
+	
+	//易宝扫码页面回调开始
+	public static String EHK_RETURN_START = "ehk_return_start";
+	//易宝扫码页面回调结束
+	public static String EHK_RETURN_END = "ehk_return_end";
+	//易宝扫码回调开始
+	public static String EHK_NOTIFY_START = "ehk_notify_start";
+	//易宝扫码回调结束
+	public static String EHK_NOTIFY_END = "ehk_notify_end";
 	//扣费开始
 	public static String COSTS_START = "costs_start";
 	//扣费结束
