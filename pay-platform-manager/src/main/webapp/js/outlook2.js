df220479118215d7a301ef743d288cd73455dcf9
devtengf <devtengf@localhost>
2017-02-23 01:03:56 +0000

    删除InitLeftMenu

--- pay-platform-manager/src/main/webapp/js/outlook2.js
+++ pay-platform-manager/src/main/webapp/js/outlook2.js
@@ -7,53 +7,12 @@ init loading
 
 
 $(function(){
-	InitLeftMenu();
 	tabClose();
 	tabCloseEven();
 
 })
 
-//初始化左侧
-function InitLeftMenu() {
-	$("#nav").accordion({animate:false});
 
-    $.each(_menus.menus, function(i, n) {
-		var menulist ='';
-		menulist +='<ul>';
-        $.each(n.menus, function(j, o) {
-			menulist += '<li><div><a ref="'+o.menuid+'" href="#" rel="' + o.url + '" ><span class="icon '+o.icon+'" >&nbsp;</span><span class="nav">' + o.menuname + '</span></a></div></li> ';
-        })
-		menulist += '</ul>';
-
-		$('#nav').accordion('add', {
-            title: n.menuname,
-            content: menulist,
-            iconCls: 'icon ' + n.icon
-        });
-
-    });
-
-	$('.easyui-accordion li a').click(function(){
-		var tabTitle = $(this).children('.nav').text();
-
-		var url = $(this).attr("rel");
-		var menuid = $(this).attr("ref");
-		var icon = getIcon(menuid,icon);
-
-		addTab(tabTitle,url,icon);
-		$('.easyui-accordion li div').removeClass("selected");
-		$(this).parent().addClass("selected");
-	}).hover(function(){
-		$(this).parent().addClass("hover");
-	},function(){
-		$(this).parent().removeClass("hover");
-	});
-
-	//选中第一个
-	var panels = $('#nav').accordion('panels');
-	var t = panels[0].panel('options').title;
-    $('#nav').accordion('select', t);
-}
 //获取左侧导航的图标
 function getIcon(menuid){
 	var icon = 'icon ';
