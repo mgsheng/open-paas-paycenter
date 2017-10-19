e35b0ea4b410b939cbc7a7d1c2cf389a263580bd
wangAmos <king_hs@sina.cn>
2017-10-19 13:37:34 +0800

    添加文件

--- pay-service/src/main/java/cn/com/open/openpaas/payservice/app/order/service/PayLoanInfoService.java
+++ pay-service/src/main/java/cn/com/open/openpaas/payservice/app/order/service/PayLoanInfoService.java
@@ -3,6 +3,6 @@ package cn.com.open.openpaas.payservice.app.order.service;
 import cn.com.open.openpaas.payservice.app.order.model.PayLoanInfo;
 
 public interface PayLoanInfoService {
-	public Boolean savePayLoanInfo(PayLoanInfo payLoanInfo);
+	public Boolean savePayLoanInfo(PayLoanInfo payLoanInfo); 
 
 }
